fced0dde8cb3d3d263735412807fb78e3b1d299d
Alex Taylor <alexmctaylorpants@gmail.com>
2017-01-14 22:48:54 -0800

    Basic GET/POST for work sessions
    - Some thoughts on this:
      `.param` is a neat trick. It can define a callback to run when a
      specific param is detected in a route. This is powerful for creating a
      pipeline of events to happen before actually processing the request
      (just like most of the middleware, it seems). It could also get really
      messy if you've got a lot of functions which handle the same route,
      since each middleware can mutate the request and response objects.
      Super powerful though.
    
      Since we can define multiple routers, we should define a router for
      the work sessions and put the relevant code in there. We shouldn't
      define too much on `app`. Same goes for the database setup.
    
      I'm not a big fan of the way the conditionals seem to be set up for
      mongoose. If error, elsif object, else? There's got to be a better
      way...

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ var mongoose = require('mongoose');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// mongo stuff
 mongoose.connect('mongodb://localhost/express-time-tracker');
 var db = mongoose.connection;
+var workSessionSchema = mongoose.Schema({
+  date: { type: Date, default: Date.now },
+  totalHours: Number,
+  rate: { type: Number, default: 25 }
+});
+
+var WorkSession = mongoose.model('WorkSession', workSessionSchema);
 
 var app = express();
 
@@ -27,9 +35,47 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+
+app.param('id', function(req, res, next, id) {
+  WorkSession.findById(id, function(err, workSession) {
+    if (err) {
+      next(err)
+    } else if (workSession) {
+      req.workSession = workSession
+      next()
+    } else {
+      next(new Error('failed to load workSession'))
+    }
+  })
+});
+
+app.get('/work_sessions', function(req, res) {
+  WorkSession.find({}, function(err, workSessions) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.send(workSessions);
+    }
+  });
+});
+
+app.get('/work_sessions/:id', function(req, res) {
+  res.send(req.workSession);
+});
+
+app.post('/work_sessions', function(req, res) {
+  const workSession = new WorkSession(req.body);
+  workSession.save(function(err, obj) {
+    if (err) {
+      res.status(422).send(err);
+    } else {
+      res.status(201).send();
+    }
+  });
+});
 
 // catch 404 and forward to error handler
+// these middleware are placed after the routes above so that any route not already handled will be caught by this function
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
